b44c05c2043aae3feabcf925f8885e7c4b35b185
lucky <gjjblucky>
2022-11-17 17:14:20 +0530

    update

--- management.js
+++ management.js
@@ -108,3 +108,212 @@ exports.Logins=async (req,res)=>{
 
 }
 
+exports.Aliases=async (req,res)=>{
+
+  const connection = await mysql.createConnection({
+    host: "103.161.43.86",
+    user: "icewarp_dba",
+    password: "Pwd@2019",
+    database: "icewarp_accounts",
+    port:3306
+    });
+
+    connection.connect()
+
+    await connection.execute('SELECT * FROM Aliases').then(result =>{
+
+      console.log(result[0])
+      res.status(200).json({status:"200 ok",data:result[0]})
+      connection.end()
+    }).catch(error=>{
+
+      console.log(error.message)
+                  res.status(404).json({status:"404 Not Found",data:error.message})
+    })
+
+
+}
+
+exports.Domains=async (req,res)=>{
+
+  const connection = await mysql.createConnection({
+    host: "103.161.43.86",
+    user: "icewarp_dba",
+    password: "Pwd@2019",
+    database: "icewarp_accounts",
+    port:3306
+    });
+
+    connection.connect()
+
+    await connection.execute('SELECT * FROM Domains').then(result =>{
+
+      console.log(result[0])
+      res.status(200).json({status:"200 ok",data:result[0]})
+      connection.end()
+    }).catch(error=>{
+
+      console.log(error.message)
+                  res.status(404).json({status:"404 Not Found",data:error.message})
+    })
+
+
+}
+
+exports.MetaData=async (req,res)=>{
+
+  const connection = await mysql.createConnection({
+    host: "103.161.43.86",
+    user: "icewarp_dba",
+    password: "Pwd@2019",
+    database: "icewarp_accounts",
+    port:3306
+    });
+
+    connection.connect()
+
+    await connection.execute('SELECT * FROM MetaData').then(result =>{
+
+      console.log(result[0])
+      res.status(200).json({status:"200 ok",data:result[0]})
+      connection.end()
+    }).catch(error=>{
+
+      console.log(error.message)
+                  res.status(404).json({status:"404 Not Found",data:error.message})
+    })
+
+
+}
+
+exports.OauthAuthorizations=async (req,res)=>{
+
+  const connection = await mysql.createConnection({
+    host: "103.161.43.86",
+    user: "icewarp_dba",
+    password: "Pwd@2019",
+    database: "icewarp_accounts",
+    port:3306
+    });
+
+    connection.connect()
+
+    await connection.execute('SELECT * FROM OauthAuthorizations').then(result =>{
+
+      console.log(result[0])
+      res.status(200).json({status:"200 ok",data:result[0]})
+      connection.end()
+    }).catch(error=>{
+
+      console.log(error.message)
+                  res.status(404).json({status:"404 Not Found",data:error.message})
+    })
+
+
+}
+
+exports.OauthRefreshTokens=async (req,res)=>{
+
+  const connection = await mysql.createConnection({
+    host: "103.161.43.86",
+    user: "icewarp_dba",
+    password: "Pwd@2019",
+    database: "icewarp_accounts",
+    port:3306
+    });
+
+    connection.connect()
+
+    await connection.execute('SELECT * FROM OauthRefreshTokens').then(result =>{
+
+      console.log(result[0])
+      res.status(200).json({status:"200 ok",data:result[0]})
+      connection.end()
+    }).catch(error=>{
+
+      console.log(error.message)
+                  res.status(404).json({status:"404 Not Found",data:error.message})
+    })
+
+
+}
+
+exports.OauthTokens=async (req,res)=>{
+
+  const connection = await mysql.createConnection({
+    host: "103.161.43.86",
+    user: "icewarp_dba",
+    password: "Pwd@2019",
+    database: "icewarp_accounts",
+    port:3306
+    });
+
+    connection.connect()
+
+    await connection.execute('SELECT * FROM OauthTokens').then(result =>{
+
+      console.log(result[0])
+      res.status(200).json({status:"200 ok",data:result[0]})
+      connection.end()
+    }).catch(error=>{
+
+      console.log(error.message)
+                  res.status(404).json({status:"404 Not Found",data:error.message})
+    })
+
+
+}
+
+exports.apisessions=async (req,res)=>{
+
+  const connection = await mysql.createConnection({
+    host: "103.161.43.86",
+    user: "icewarp_dba",
+    password: "Pwd@2019",
+    database: "icewarp_accounts",
+    port:3306
+    });
+
+    connection.connect()
+
+    await connection.execute('SELECT * FROM apisessions').then(result =>{
+
+      console.log(result[0])
+      res.status(200).json({status:"200 ok",data:result[0]})
+      connection.end()
+    }).catch(error=>{
+
+      console.log(error.message)
+                  res.status(404).json({status:"404 Not Found",data:error.message})
+    })
+
+
+}
+
+exports.demo=async (req,res)=>{
+
+  const connection = await mysql.createConnection({
+    host: "103.161.43.86",
+    user: "icewarp_dba",
+    password: "Pwd@2019",
+    database: "icewarp_accounts",
+    port:3306
+    });
+
+    connection.connect()
+
+    await connection.execute('SELECT * FROM demo').then(result =>{
+
+      console.log(result[0])
+      res.status(200).json({status:"200 ok",data:result[0]})
+      connection.end()
+    }).catch(error=>{
+
+      console.log(error.message)
+                  res.status(404).json({status:"404 Not Found",data:error.message})
+    })
+
+
+}
+
+
--- router/management.js
+++ router/management.js
@@ -8,5 +8,14 @@ router.get('/userAccessData',domainControllers.USERACCESS);
 router.get('/authClientsData',domainControllers.OauthClients);
 router.get('/loginsData',domainControllers.Logins);
 
+router.get('/AliasesData',domainControllers.Aliases);
+router.get('/DomainsData',domainControllers.Domains);
+router.get('/MetaDataData',domainControllers.MetaData);
+router.get('/OauthAuthorizationsData',domainControllers.OauthAuthorizations);
+router.get('/OauthRefreshTokensData',domainControllers.OauthRefreshTokens);
+router.get('/OauthTokensData',domainControllers.OauthTokens);
+router.get('/apisessionsData',domainControllers.apisessions);
+router.get('/demoData',domainControllers.demo);
+
 
 module.exports = router;
