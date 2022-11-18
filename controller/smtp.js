4bc011d8ab6f393cafe5c14b225af7eae0e58597
lucky <gjjblucky>
2022-11-18 14:25:32 +0530

    update

--- controller/smtp.js
+++ controller/smtp.js
@@ -19,29 +19,29 @@ const x='public-folders@icewarpdemo.com'
   // const name=await connection2.execute('SELECT U_Name FROM Users LIMIT 3');
   console.log(name)
   
-  const sendLimit=await connection2.execute('SELECT U_MegabyteSendLimit FROM Users LIMIT 3');
-  console.log(sendLimit)
+  // const sendLimit=await connection2.execute('SELECT U_MegabyteSendLimit FROM Users LIMIT 3');
+  // console.log(sendLimit)
   
-  const mailBox=await connection2.execute('SELECT U_MaxBoxSize FROM Users LIMIT 3');
-  console.log(mailBox)
+  // const mailBox=await connection2.execute('SELECT U_MaxBoxSize FROM Users LIMIT 3');
+  // console.log("mailBox",mailBox[0])
   
-  const externalMail=await connection2.execute('SELECT U_UseRemoteAddress FROM Users LIMIT 3');
-  console.log(externalMail)
+  // const externalMail=await connection2.execute('SELECT U_UseRemoteAddress FROM Users LIMIT 3');
+  // console.log("externalMail",externalMail[0])
   
-  const lastLogin=await connection2.execute('SELECT UA_LastLogin FROM UserAccess LIMIT 3')
-  console.log(lastLogin)
+  // const lastLogin=await connection2.execute('SELECT UA_LastLogin FROM UserAccess LIMIT 3')
+  // console.log("lastlogin",lastLogin[0])
   
-  const lastSent=await connection2.execute('SELECT UA_LastSent FROM UserAccess LIMIT 3')
-  console.log(lastSent)
+  // const lastSent=await connection2.execute('SELECT UA_LastSent FROM UserAccess LIMIT 3')
+  // console.log("lastSend",lastSent[0])
   
-  const lastReceived=await connection2.execute('SELECT UA_LastReceived FROM UserAccess LIMIT 3')
-  console.log(lastReceived)
+  // const lastReceived=await connection2.execute('SELECT UA_LastReceived FROM UserAccess LIMIT 3')
+  // console.log("lastReceived",lastReceived[0])
   
-  const disabledAccount=await connection2.execute('SELECT U_AccountDisabled FROM UserAccess WHERE U_AccountDisabled = 0 ')
-  console.log(disabledAccount)
+  // const disabledAccount=await connection2.execute('SELECT U_AccountDisabled FROM Users WHERE U_AccountDisabled = 0 ')
+  // console.log("disable",disabledAccount[0])
   
-  const enabledAccount=await connection2.execute('SELECT U_AccountDisabled FROM UserAccess WHERE U_AccountDisabled = 1')
-  console.log(enabledAccount)
+  // const enabledAccount=await connection2.execute('SELECT U_AccountDisabled FROM Users WHERE U_AccountDisabled = 1')
+  // console.log("enable",enabledAccount[0])
 
 
 }
