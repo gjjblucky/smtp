d578b388b7990592c01786d009b6c3361ddc173e
lucky <gjjblucky>
2022-11-18 14:34:55 +0530

    update smtp

--- controller/smtp.js
+++ controller/smtp.js
@@ -16,33 +16,8 @@ const x='public-folders@icewarpdemo.com'
   const connection2 = await mysql.createConnection(config2);
 
   const name=await connection2.execute(`SELECT * FROM Users WHERE U_Alias+@+U_Domain=x`);
-  // const name=await connection2.execute('SELECT U_Name FROM Users LIMIT 3');
-  console.log(name)
-  
-  // const sendLimit=await connection2.execute('SELECT U_MegabyteSendLimit FROM Users LIMIT 3');
-  // console.log(sendLimit)
-  
-  // const mailBox=await connection2.execute('SELECT U_MaxBoxSize FROM Users LIMIT 3');
-  // console.log("mailBox",mailBox[0])
-  
-  // const externalMail=await connection2.execute('SELECT U_UseRemoteAddress FROM Users LIMIT 3');
-  // console.log("externalMail",externalMail[0])
-  
-  // const lastLogin=await connection2.execute('SELECT UA_LastLogin FROM UserAccess LIMIT 3')
-  // console.log("lastlogin",lastLogin[0])
-  
-  // const lastSent=await connection2.execute('SELECT UA_LastSent FROM UserAccess LIMIT 3')
-  // console.log("lastSend",lastSent[0])
-  
-  // const lastReceived=await connection2.execute('SELECT UA_LastReceived FROM UserAccess LIMIT 3')
-  // console.log("lastReceived",lastReceived[0])
-  
-  // const disabledAccount=await connection2.execute('SELECT U_AccountDisabled FROM Users WHERE U_AccountDisabled = 0 ')
-  // console.log("disable",disabledAccount[0])
-  
-  // const enabledAccount=await connection2.execute('SELECT U_AccountDisabled FROM Users WHERE U_AccountDisabled = 1')
-  // console.log("enable",enabledAccount[0])
 
+  console.log(name)
 
 }
 manage();
